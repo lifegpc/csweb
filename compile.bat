5eb3d3dd10f1c5e452e63166c52c7e72b3424683
lifegpc <g1710431395@gmail.com>
2021-03-13 07:47:40 +0800

    update

--- compile.bat
+++ compile.bat
@@ -3,5 +3,5 @@ SETLOCAL
 set py=python
 set pyt=%py% compile.py
 %pyt% captcha2.js xhr.js i18n.js sendMsgToMe.js
-%pyt% -W undefinedVars -t salt.js xhr.js wplink.js
+%pyt% -t salt.js xhr.js wplink.js
 ENDLOCAL
