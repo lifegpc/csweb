1001c068f88f6c6936e75aa28b465141325603e1
lifegpc <g1710431395@gmail.com>
2021-04-04 22:03:57 +0800

    update drawBagel

--- drawBagel.py
+++ drawBagel.py
@@ -27,26 +27,30 @@ def textwidth(text: str, fontSize: int, font: str):
 
 def drawBagel(text: str, text2: str, fontSize: int = 13,
               textColor: str = '#FFF', leftColor: str = '#555',
-              rightColor: str = '#08C', spacing: int = 10) -> str:
+              rightColor: str = '#08C', spacing: int = 10,
+              height: int = 20) -> str:
     spacing = max(spacing, 3)
-    fontSize = max(10, max(15, fontSize))
+    height = max(20, height)
+    fontSize = max(round(height * 0.5), max(round(height * 0.75), fontSize))
     s = settings()
     s.ReadSettings()
     t1w = textwidth(text, fontSize, s.defaultSvgFont)
     t2w = textwidth(text2, fontSize, s.defaultSvgFont)
     zw = 4 * spacing + t1w + t2w
-    d = Drawing(zw, 20)
+    d = Drawing(zw, height)
     m = Mask(id="m")
     d.append(m)
-    m.append(Rectangle(0, 0, zw, 20, fill=textColor, rx="3", ry="3"))
+    rx = round(height * 0.15)
+    m.append(Rectangle(0, 0, zw, height, fill='#FFF', rx=rx, ry=rx))
     g1 = Group(mask="url(#m)")
-    g1.append(Rectangle(0, 0, 2 * spacing + t1w, 20, fill=leftColor))
-    g1.append(Rectangle(2 * spacing + t1w, 0, zw, 20, fill=rightColor))
+    g1.append(Rectangle(0, 0, 2 * spacing + t1w, height, fill=leftColor))
+    g1.append(Rectangle(2 * spacing + t1w, 0, zw, height, fill=rightColor))
     d.append(g1)
     g2 = Group(aria_hidden=True, fill=textColor, text_anchor='start',
                font_family='sans-serif')
-    g2.append(Text(text, fontSize, spacing, 20 - fontSize, textLength=t1w))
-    g2.append(Text(text2, fontSize, 3 * spacing + t1w, 20 - fontSize, textLength=t2w))
+    g2.append(Text(text, fontSize, spacing, height - fontSize, textLength=t1w))
+    g2.append(Text(text2, fontSize, 3 * spacing + t1w, height - fontSize,
+                   textLength=t2w))
     d.append(g2)
     return d.asSvg()
 
@@ -55,9 +59,27 @@ class DrawBagel:
     def GET(self):
         try:
             text = web.input().get("t")
+            if text is None:
+                text = web.input().get("text")
             text2 = web.input().get("t2")
+            if text2 is None:
+                text = web.input().get("text2")
             spacing: str = web.input().get("spacing")
-            fontSize: str = web.input().get("fontSize")
+            fontSize: str = web.input().get("fs")
+            if fontSize is None:
+                fontSize = web.input().get("fontSize")
+            height: str = web.input().get("h")
+            if height is None:
+                height = web.input().get("height")
+            textColor: str = web.input().get("tc")
+            if textColor is None:
+                textColor = web.input().get("textColor")
+            leftColor: str = web.input().get("lc")
+            if leftColor is None:
+                leftColor = web.input().get("leftColor")
+            rightColor: str = web.input().get("rc")
+            if rightColor is None:
+                rightColor = web.input().get("rightColor")
             if text is None or text2 is None:
                 return 'Must have t and t2 parameters.'
             args = {}
@@ -65,6 +87,14 @@ class DrawBagel:
                 args["spacing"] = int(spacing)
             if fontSize is not None and fontSize.isnumeric():
                 args["fontSize"] = int(fontSize)
+            if height is not None and height.isnumeric():
+                args["height"] = int(height)
+            if textColor is not None and textColor != '':
+                args["textColor"] = textColor
+            if leftColor is not None and leftColor != '':
+                args["leftColor"] = leftColor
+            if rightColor is not None and rightColor != '':
+                args["rightColor"] = rightColor
             svg = drawBagel(text, text2, **args)
             web.header('Content-Type', 'image/svg+xml')
             web.header('Cache-Control', 'public, max-age=600')
