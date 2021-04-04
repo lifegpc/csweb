7ee70403a2236a23b4cb18da80536fbf6f5c2a2c
lifegpc <g1710431395@gmail.com>
2021-04-04 22:52:52 +0800

    add bagel support for clearurl

--- clearurl.py
+++ clearurl.py
@@ -6,6 +6,7 @@ from constants import jsonsep
 from json import dumps, load as loadJson
 from hashl import sha256
 from re import search
+from drawBagel import drawBagel
 
 
 class ClearUrl:
@@ -14,11 +15,15 @@ class ClearUrl:
             web.header("Content-Type", "application/json; charset=utf-8")
             s = settings()
             s.ReadSettings()
+            bagel = web.input().get("bagel")
+            bagel = False if bagel is None else True
             nod = web.input().get("noda")
             nod = False if nod is None else True
+            ol = 0
             if not nod:
                 r = get(s.clearUrlOrigin)
                 r = r.json()
+                ol = len(r['providers'])
             else:
                 r = {"providers": {}}
             li = s.clearUrlCustomList
@@ -29,9 +34,12 @@ class ClearUrl:
                 return sha256(t) if h else t
             ind: str = web.input().get("t")
             ind = ind.split(',') if ind is not None and ind != '' else None
+            vt = []
             for i in li:
                 if ind is not None and i not in ind:
                     continue
+                elif ind is not None:
+                    vt.append(i)
                 with open(li[i], 'r', encoding='utf8') as f:
                     j = loadJson(f)
                     if 'providers' in j:
@@ -49,7 +57,26 @@ class ClearUrl:
                                 ke = f"{te}_{z}"
                             r['providers'][ke] = j['providers'][m]
             t = dumps(r, separators=jsonsep)
-            return sha256(t) if h else t
+            if h:
+                return sha256(t)
+            elif bagel:
+                lt = str(len(r["providers"].keys()))
+                adm = []
+                if int(lt) == ol:
+                    adm.append('no any custom rules')
+                if int(lt) != ol and ind is not None:
+                    adm.append('types: ' + '; '.join(vt))
+                if not nod:
+                    adm.append(f'include {ol} offical rules')
+                at = ', '.join(adm)
+                at = '' if at == '' else f" ({at})"
+                rt = f'custom rules{at}'
+                svg = drawBagel(lt, rt, leftColor='#555', rightColor='#007ec6',
+                                spacing=3)
+                web.header('Content-Type', 'image/svg+xml')
+                return svg
+            else:
+                return t
         except:
             web.HTTPError('500 Internal Server Error')
             try:
