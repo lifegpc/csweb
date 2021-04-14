4531543ac1b25a7ef1662bf2b9100d876d943103
lifegpc <g1710431395@gmail.com>
2021-04-14 14:55:54 +0800

    fix bug

--- sign.py
+++ sign.py
@@ -1,19 +1,11 @@
 from hashl import sha512
-import web
-from urllib.parse import quote_plus as q, parse_qs
+from urllib.parse import quote_plus as q
 from rawdata import getInputStrDict
 SHA512_TYPE = 1
 
 
 def verifySign(sercet: str, t: int = SHA512_TYPE):
-    query: str = web.ctx.query
-    if query is not None and len(query) > 0:
-        query = query[1:]
-    elif query is None:
-        query = ''
-    qd = parse_qs(query, keep_blank_values=True)
-    if web.ctx.env["REQUEST_METHOD"].lower() == "post":
-        qd = getInputStrDict(qd)
+    qd = getInputStrDict()
     sign = qd["sign"][0] if "sign" in qd else None
     if sign is None or sign == '':
         return False
