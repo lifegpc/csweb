985640e3f861665849fb4b08743fb03903e44139
lifegpc <g1710431395@gmail.com>
2021-05-22 20:17:35 +0800

    use a sparate function to generate sign

--- sign.py
+++ sign.py
@@ -1,14 +1,10 @@
 from hashl import sha512
-from urllib.parse import quote_plus as q
-from rawdata import getInputStrDict
+from urllib.parse import quote_plus as q, parse_qs
+from rawdata import getInputStrDict, ParseQsResult
 SHA512_TYPE = 1
 
 
-def verifySign(sercet: str, t: int = SHA512_TYPE):
-    qd = getInputStrDict()
-    sign = qd["sign"][0] if "sign" in qd else None
-    if sign is None or sign == '':
-        return False
+def genSign(sercet: str, qd: ParseQsResult, t: int = SHA512_TYPE):
     para = ''
     keys = qd.keys()
     keys = sorted(keys)
@@ -22,4 +18,17 @@ def verifySign(sercet: str, t: int = SHA512_TYPE):
         h = sha512(para)
     else:
         h = sha512(para)
+    return h
+
+
+def verifySign(sercet: str, onlyQuery: bool = False, t: int = SHA512_TYPE):
+    if onlyQuery:
+        import web
+        e = web.ctx.env.copy()
+        qs = e['QUERY_STRING']
+    qd = parse_qs(qs) if onlyQuery else getInputStrDict()
+    sign = qd["sign"][0] if "sign" in qd else None
+    if sign is None or sign == '':
+        return False
+    h = genSign(sercet, qd, t)
     return True if h == sign else False
