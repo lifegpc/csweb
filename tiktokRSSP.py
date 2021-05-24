2658deb653ccb51785fd3775066eab476a3e30f3
lifegpc <g1710431395@gmail.com>
2021-05-24 17:18:24 +0800

    minor imporvement

--- tiktokRSSP.py
+++ tiktokRSSP.py
@@ -2,6 +2,7 @@ from sign import genSign
 from urllib.parse import urlencode
 import web
 from json import dumps
+from constants import jsonsep
 
 
 def genUrl(origin: str, serects: str, d: dict = None, ref: str = None) -> str:
@@ -10,11 +11,11 @@ def genUrl(origin: str, serects: str, d: dict = None, ref: str = None) -> str:
     li = [('t', origin)]
     if d is not None:
         if 'cookies' in d:
-            t = dumps(d['cookies'])
+            t = dumps(d['cookies'], ensure_ascii=False, separators=jsonsep)
             p['c'] = [t]
             li.append(('c', t))
         if 'headers' in d:
-            t = dumps(d['headers'])
+            t = dumps(d['headers'], ensure_ascii=False, separators=jsonsep)
             p['h'] = [t]
             li.append(('h', t))
     if ref is not None:
