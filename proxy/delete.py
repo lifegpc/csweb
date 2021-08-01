3fe8686bbe6f0194fee3cac711d3023f8ed95814
lifegpc <g1710431395@gmail.com>
2021-08-01 16:10:18 +0800

    proxy: add cors support for add, delete, deleteAll

--- proxy/delete.py
+++ proxy/delete.py
@@ -19,57 +19,82 @@ from sign import verifySign  # noqa: E402
 
 
 class ProxyDelete:
+    def __init__(self):
+        self._method = None
+
     def GET(self):
+        self._method = 'GET'
         return self.POST()
 
     def POST(self):
+        if self._method is None:
+            self._method = 'POST'
         web.header("Content-Type", "application/json; charset=utf-8")
         try:
+            callback = None
+            if self._method == 'GET':
+                callback = web.input().get("callback")
+                if callback is not None and callback != '':
+                    web.header("Content-Type",
+                               "application/javascript; charset=utf-8")
+                else:
+                    callback = None
+            cors = web.input().get("cors")
+            if cors is not None:
+                web.header('Access-Control-Allow-Origin', '*')
             s = settings()
             s.ReadSettings()
             sg = s.proxyAPISecrets
             if sg is None:
-                return dumps({"code": -500, "msg":
-                              "proxyAPISecrets must be set in settings."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -500, "msg":
+                           "proxyAPISecrets must be set in settings."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             if not verifySign(sg):
-                return dumps({"code": -401, "msg": "Unauthorized"},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -401, "msg": "Unauthorized"},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             t = web.input().get("t")
             if t is None:
-                return dumps({"code": -1, "msg":
-                              "current time(t) is needed."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -1, "msg": "current time(t) is needed."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             try:
                 t = int(t)
             except:
-                return dumps({"code": -2, "msg":
-                              "current time(t) must be a integer."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -2, "msg":
+                           "current time(t) must be a integer."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             nt = round(time())
             if nt > (t + 300) or t < (t - 300):
-                return dumps({"code": -3, "msg":
-                              "Emm. Seems the current time is not right."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -3, "msg":
+                           "Emm. Seems the current time is not right."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             idd = web.input().get("id")
             if idd is None:
-                return dumps({"code": -4, "msg": "id is needed."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -4, "msg": "id is needed."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             act = web.input().get("a")
             if act is None or act == '':
                 act = web.input().get("action")
             if act is None or act == '':
-                return dumps({"code": -5, "msg":
-                              "action type (a/action) is needed."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -5, "msg":
+                           "action type (a/action) is needed."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             if act != 'delete':
-                return dumps({"code": -6, "msg":
-                              "action type (a/action) must be 'delete'."},
-                             ensure_ascii=False, separators=jsonsep)
+                d = dumps({"code": -6, "msg":
+                           "action type (a/action) must be 'delete'."},
+                          ensure_ascii=False, separators=jsonsep)
+                return d if callback is None else f'{callback}({d})'
             db = ProxyDb()
             r = db.delete_proxy(idd)
-            return dumps({"code": 0, "result": r}, ensure_ascii=False,
-                         separators=jsonsep)
+            d = dumps({"code": 0, "result": r}, ensure_ascii=False,
+                      separators=jsonsep)
+            return d if callback is None else f'{callback}({d})'
         except:
             t = ''
             try:
@@ -81,6 +106,11 @@ class ProxyDelete:
                 pass
             return dumps({"code": -500, "msg": t}, ensure_ascii=False,
                          separators=jsonsep)
+
+    def OPTIONS(self):
+        from cors import allowCors
+        allowCors()
+        return ''
 
 
 if m:
