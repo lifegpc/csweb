6cdc21f69b9224d6862118be9f61ac9351f6d32d
lifegpc <g1710431395@gmail.com>
2021-11-24 23:06:55 +0800

    add more options

--- js(origin)/tools/md_to_html.js
+++ js(origin)/tools/md_to_html.js
@@ -32,6 +32,13 @@ function main() {
     let opt3 = document.getElementById('opt3');
     let opt4 = document.getElementById('opt4');
     let opt5 = document.getElementById('opt5');
+    let opt6 = document.getElementById('opt6');
+    let opt7 = document.getElementById('opt7');
+    let opt8 = document.getElementById('opt8');
+    let opt9 = document.getElementById('opt9');
+    let opt10 = document.getElementById('opt10');
+    let opt11 = document.getElementById('opt11');
+    let opt12 = document.getElementById('opt12');
     let showopt = false;
     let opts = 0;
     optb.addEventListener('click', () => {
@@ -63,6 +70,34 @@ function main() {
     })
     opt5.addEventListener('input', () => {
         opts = handleopts(opt5, _cmark_gfm.CMARK_OPT_VALIDATE_UTF8, opts);
+        console.log(opts);
+    })
+    opt6.addEventListener('input', () => {
+        opts = handleopts(opt6, _cmark_gfm.CMARK_OPT_SMART, opts);
+        console.log(opts);
+    })
+    opt7.addEventListener('input', () => {
+        opts = handleopts(opt7, _cmark_gfm.CMARK_OPT_GITHUB_PRE_LANG, opts);
+        console.log(opts);
+    })
+    opt8.addEventListener('input', () => {
+        opts = handleopts(opt8, _cmark_gfm.CMARK_OPT_LIBERAL_HTML_TAG, opts);
+        console.log(opts);
+    })
+    opt9.addEventListener('input', () => {
+        opts = handleopts(opt9, _cmark_gfm.CMARK_OPT_FOOTNOTES, opts);
+        console.log(opts);
+    })
+    opt10.addEventListener('input', () => {
+        opts = handleopts(opt10, _cmark_gfm.CMARK_OPT_STRIKETHROUGH_DOUBLE_TILDE, opts);
+        console.log(opts);
+    })
+    opt11.addEventListener('input', () => {
+        opts = handleopts(opt11, _cmark_gfm.CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES, opts);
+        console.log(opts);
+    })
+    opt12.addEventListener('input', () => {
+        opts = handleopts(opt12, _cmark_gfm.CMARK_OPT_FULL_INFO_STRING, opts);
         console.log(opts);
     })
 }
