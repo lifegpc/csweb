78b5a5a1bf3d1886fe364d8e9b60bdd9670a2e72
lifegpc <g1710431395@gmail.com>
2022-01-20 15:16:50 +0800

    update /proxy/manage

--- js(origin)/input_list.js
+++ js(origin)/input_list.js
@@ -5,10 +5,12 @@ class InputList {
     /**
      * Initialize Input List
      * @param {Element} base The Element to render the list
-     * @param {(modified: {k: string, v: string} | undefined, data: Object<string, {k: string, v: string}>)=>void | undefined} update The callback function when data was updated
+     * @param {(modified: {k: string, v: string} | undefined, data: Object<string, string>)=>void | undefined} update The callback function when data was updated
      * @param {number | undefined} minWidth The min size of key inputbox.
+     * @param {Object<string, string> | undefined} initData The data to put after initailzed.
+     * @param {boolean | undefined} allowEdit Whether to allow edit. (Default: true)
      */
-    constructor(base, update, minWidth) {
+    constructor(base, update, minWidth, initData, allowEdit) {
         /**@type {Element} The Element to render the list*/
         this.base = base;
         /**@type {Object<string, {k: string, v: string}>} The location to store data*/
@@ -21,26 +23,39 @@ class InputList {
         this.count = 0;
         /**@type {Object<string, HTMLInputElement>}*/
         this.minusb = {};
+        /**@type {Object<string, HTMLInputElement>}*/
+        this.addb = {};
         /**@type {Number} The min size of key inputbox.*/
         this.minWidth = minWidth ? minWidth : 50;
-        this.add()
+        this.allowEdit = allowEdit != undefined ? allowEdit : true;
+        if (initData != undefined) {
+            this.fromData(initData);
+        } else {
+            this.add();
+        }
     }
     /**
      * Add a new pair to specified location
      * @param {Number | undefined} index The location want to insert. If not specified, insert to the last.
+     * @param {string | undefined} key The inital key
+     * @param {string | undefined} value The intial value
      */
-    add(index) {
+    add(index, key, value) {
+        if (!key) key = "";
+        if (!value) value = "";
         let div = document.createElement('div');
         div.setAttribute('i', this.id);
-        let data = { k: "", v: "" };
+        let data = { k: key, v: value };
         this._data[this.id] = data;
         let inp1 = document.createElement('input');
         inp1.style.width = "20%";
         inp1.style.minWidth = this.minWidth + "px";
+        inp1.value = key;
         inp1.addEventListener('input', () => {
             data.k = inp1.value;
-            if (this.update) this.update(data, this._data);
+            if (this.update) this.update(data, this.toObject());
         })
+        inp1.disabled = !this.allowEdit;
         div.append(inp1);
         div.append(" = ");
         let inp2 = document.createElement('input');
@@ -48,8 +63,10 @@ class InputList {
         inp2.style.minWidth = (this.minWidth * 3) + "px";
         inp2.addEventListener('input', () => {
             data.v = inp2.value;
-            if (this.update) this.update(data, this._data);
+            if (this.update) this.update(data, this.toObject());
         })
+        inp2.value = value;
+        inp2.disabled = !this.allowEdit;
         div.append(inp2);
         let minus = document.createElement('input');
         minus.type = "button";
@@ -62,6 +79,7 @@ class InputList {
         let add = document.createElement('input');
         add.type = "button";
         add.value = "+";
+        this.addb[this.id] = add;
         ((inputi) => {
             add.addEventListener('click', () => {
                 this.add(inputi);
@@ -82,21 +100,47 @@ class InputList {
         this.id++;
         this.count++;
         this.checkButtonStatus();
-        if (this.update) this.update(data, this._data);
+        if (this.update) this.update(data, this.toObject());
     }
     checkButtonStatus() {
         let l = Object.getOwnPropertyNames(this.minusb);
         let c = l.length;
         l.forEach((key) => {
-            this.minusb[key].disabled = c < 2;
+            this.minusb[key].disabled = !this.allowEdit || c < 2;
+        })
+        Object.getOwnPropertyNames(this.addb).forEach((key) => {
+            this.addb[key].disabled = !this.allowEdit;
         })
     }
     data() {
         return this._data;
     }
+    /**
+     * Destory the list.  
+     * Note. No update will be recived.
+     */
     destory() {
+        this.update = undefined;
         for (let i = 0; i < this.base.childElementCount; i++) {
             this.removeDiv(this.base.children[i]);
+        }
+        this.id = 0;
+    }
+    /**
+     * Recrate list with data
+     * @param {Object<string, string>} data Data
+     */
+    fromData(data) {
+        if (this.count) {
+            let update = this.update;
+            this.destory();
+            this.update = update;
+        }
+        Object.getOwnPropertyNames(data).forEach((key) => {
+            this.add(undefined, key, data[key]);
+        })
+        if (!this.count) {
+            this.add();
         }
     }
     isEmpty() {
@@ -115,10 +159,11 @@ class InputList {
         let ind = parseInt(div.getAttribute("i"));
         div.parentElement.removeChild(div);
         delete this.minusb[ind];
+        delete this.addb[ind];
         delete this._data[ind];
         this.count--;
         this.checkButtonStatus();
-        if (this.update) this.update(undefined, this._data);
+        if (this.update) this.update(undefined, this.toObject());
     }
     /**
      * @returns {Object<string, string>}
--- js(origin)/proxy/manage.js
+++ js(origin)/proxy/manage.js
@@ -28,6 +28,27 @@ window.addEventListener('load', () => {
     let i18n = window['i18n'];
     /**@type {Array<string>}*/
     let data = [];
+    /**@type {HTMLInputElement} Button: First page*/
+    let fp = document.getElementById('fp');
+    /**@type {HTMLInputElement} Button: Previous page*/
+    let pp = document.getElementById('pp');
+    /**@type {HTMLInputElement} Button: Next page*/
+    let np = document.getElementById('np');
+    /**@type {HTMLInputElement} Button: Last page*/
+    let lp = document.getElementById('lp');
+    /**@type {HTMLDivElement} Page <num>, <total> in total.*/
+    let pn = document.getElementById('pn');
+    let page_now = 0;
+    /**@type {Array<{c: InputList, h: InputList}}*/
+    let inputs_list = [];
+    /**@type {HTMLTableSectionElement}*/
+    let listd = document.getElementById('list');
+    /**@type {HTMLTemplateElement}*/
+    let listt = document.getElementById('listt');
+    /**@type {Object<string, {cookies: Object<string, string>, headers: Object<string, string>}>}*/
+    let datas = {};
+    /**@type {HTMLInputElement} Button: Delete all proxies*/
+    let dab = document.getElementById('da');
     /**
      * The callback when serect is changed
      * @param {string} str The new secret
@@ -37,14 +58,121 @@ window.addEventListener('load', () => {
             glb.disabled = false;
             addb.disabled = false;
             caddb.disabled = false;
+            dab.disabled = false;
         } else {
             glb.disabled = true;
             addb.disabled = true;
             caddb.disabled = true;
+            dab.disabled = true;
         }
     }
     function get_time() {
         return (Math.floor(new Date().getTime() / 1000)).toString();
+    }
+    /**
+     * Render proxy
+     * @param {string} id The ID of proxy
+     */
+    function render_proxy(id) {
+        /**@type {HTMLInputElement}*/
+        let t = listt.content.firstElementChild.cloneNode(true);
+        /**@type {HTMLTableCellElement}*/
+        let ide = t.querySelector('.id');
+        ide.innerText = id;
+        let d = { c: undefined, h: undefined };
+        /**@type {HTMLInputElement}*/
+        let sc = t.querySelector('.sc');
+        let cookies = t.querySelector('.cookies');
+        /**@type {HTMLInputElement}*/
+        let sh = t.querySelector('.sh');
+        let headers = t.querySelector('.headers');
+        let del = t.querySelector('.del');
+        /**
+         * @param {Element} source
+         * @param {HTMLInputElement} button Button
+         * @param {Object<string, string>} data Data
+         * @returns {InputList}
+        */
+        function create_list(source, button, data) {
+            let tmp = new InputList(source, undefined, undefined, data, false);
+            button.style.display = 'none';
+            return tmp;
+        }
+        /**
+         * @param {()=>void} callback 
+         */
+        function get_data(callback) {
+            let p = { "a": "get", "t": get_time(), "id": id };
+            p['sign'] = genGetSign(p, keyinp.value);
+            post('/proxy/get', p, (da) => {
+                let d = JSON.parse(da);
+                let code = d['code'];
+                if (code == -500) {
+                    console.warn('Server error: ', d['msg']);
+                    alert(i18n['SEVERR']);
+                } else if (code == -401) {
+                    alert(i18n['UNAUTH']);
+                } else if (code == 0) {
+                    datas[id] = {};
+                    datas[id].cookies = JSON.parse(d['result']['cookies'] || "{}");
+                    datas[id].headers = JSON.parse(d['result']['headers'] || "{}");
+                    callback();
+                } else {
+                    alert(d['msg']);
+                    if (code == -5) {
+                        get_proxy_list();
+                    }
+                }
+            })
+        }
+        sc.addEventListener('click', () => {
+            if (!d.c) {
+                if (datas.hasOwnProperty(id)) {
+                    d.c = create_list(cookies, sc, datas[id].cookies);
+                } else {
+                    get_data(() => {
+                        d.c = create_list(cookies, sc, datas[id].cookies);
+                    })
+                }
+            }
+        })
+        sh.addEventListener('click', () => {
+            if (!d.h) {
+                if (datas.hasOwnProperty(id)) {
+                    d.h = create_list(headers, sh, datas[id].headers);
+                } else {
+                    get_data(() => {
+                        d.h = create_list(headers, sh, datas[id].headers);
+                    })
+                }
+            }
+        })
+        del.addEventListener('click', () => {
+            if (!confirm(i18n['CODEL'] + '\n' + i18n['UNDOW'])) return;
+            let p = { "a": "delete", "t": get_time(), "id": id };
+            p['sign'] = genGetSign(p, keyinp.value);
+            post("/proxy/delete", p, (da) => {
+                let d = JSON.parse(da);
+                let code = d['code'];
+                if (code == -500) {
+                    console.warn('Server error: ', d['msg']);
+                    alert(i18n['SEVERR']);
+                } else if (code == -401) {
+                    alert(i18n['UNAUTH']);
+                } else if (code == 0) {
+                    if (d['result']) {
+                        alert(i18n['SUCDEL']);
+                        get_proxy_list();
+                    } else {
+                        alert(i18n['FADEL']);
+                    }
+                } else {
+                    alert(d['msg']);
+                }
+            })
+        })
+        inputs_list.push(d);
+        listd.append(t);
     }
     /**
      * Render proxy list
@@ -52,12 +180,52 @@ window.addEventListener('load', () => {
      */
     function render_proxy_list(page) {
         page = Math.floor(page);
+        let total_page = Math.ceil(data.length / 10);
+        if (inputs_list.length) {
+            inputs_list.forEach((d) => {
+                if (d.c) d.c.destory();
+                if (d.h) d.h.destory();
+            })
+            inputs_list = [];
+        }
+        if (total_page <= 0) {
+            pn.innerText = i18nReplace(i18n['PN'], { "num": 0, "total": 0 });
+            fp.disabled = true;
+            pp.disabled = true;
+            np.disabled = true;
+            lp.disabled = true;
+            listd.innerHTML = '';
+            return;
+        }
+        if (page <= 0) page = 1;
+        if (page > total_page) page = total_page;
+        pn.innerText = i18nReplace(i18n['PN'], { "num": page, "total": total_page });
+        if (page != 1) {
+            fp.disabled = false;
+            pp.disabled = false;
+        } else {
+            fp.disabled = true;
+            pp.disabled = true;
+        }
+        if (page != total_page) {
+            lp.disabled = false;
+            np.disabled = false;
+        } else {
+            lp.disabled = true;
+            np.disabled = true;
+        }
+        listd.innerHTML = '';
+        page_now = page;
+        let data_list = data.slice((page - 1) * 10, Math.min(page * 10, data.length));
+        data_list.forEach((d) => {
+            render_proxy(d);
+        })
     }
     function get_proxy_list() {
-        let p = {'a': "list", 't': get_time()};
+        let p = { 'a': "list", 't': get_time() };
         p['sign'] = genGetSign(p, keyinp.value);
-        post('/proxy/list', p, (data) => {
-            let d = JSON.parse(data);
+        post('/proxy/list', p, (da) => {
+            let d = JSON.parse(da);
             let code = d['code'];
             if (code == -500) {
                 console.warn('Server error: ', d['msg']);
@@ -66,6 +234,7 @@ window.addEventListener('load', () => {
                 alert(i18n['UNAUTH']);
             } else if (code == 0) {
                 data = d['result'];
+                datas = {};
                 render_proxy_list(0);
             } else {
                 alert(d['msg']);
@@ -88,22 +257,16 @@ window.addEventListener('load', () => {
     })
     caddb.addEventListener('click', () => {
         if (!id.value.length) {
-            alert(i18nReplace(i18n['REQ'], {'a': 'ID'}));
+            alert(i18nReplace(i18n['REQ'], { 'a': 'ID' }));
             return;
         } else if (input_cookies.isEmpty() && input_headers.isEmpty()) {
-            alert(i18nReplace(i18n['REQ'], {'a': i18nReplace(i18n['OR'], {'a': 'Cookies', 'b': 'Headers'})}))
+            alert(i18nReplace(i18n['REQ'], { 'a': i18nReplace(i18n['OR'], { 'a': 'Cookies', 'b': 'Headers' }) }))
             return;
         }
-        let p = {"a": "add", "t": get_time(), "id": id.value}
-        if (!input_cookies.isEmpty()) {
-            p['c'] = JSON.stringify(input_cookies.toObject());
-        }
-        if (!input_headers.isEmpty()) {
-            p['h'] = JSON.stringify(input_headers.toObject());
-        }
+        let p = { "a": "exists", "t": get_time(), "id": id.value }
         p['sign'] = genGetSign(p, keyinp.value);
-        post("/proxy/add", p, (data) => {
-            let d = JSON.parse(data);
+        post("/proxy/exists", p, (da) => {
+            let d = JSON.parse(da);
             let code = d['code'];
             if (code == -500) {
                 console.warn('Server error: ', d['msg']);
@@ -111,7 +274,69 @@ window.addEventListener('load', () => {
             } else if (code == -401) {
                 alert(i18n['UNAUTH']);
             } else if (code == 0) {
-                get_proxy_list();
+                if (d['result']) {
+                    if (!confirm(i18n['COADD'] + '\n' + i18n['UNDOW'])) return;
+                }
+                let p = { "a": "add", "t": get_time(), "id": id.value }
+                if (!input_cookies.isEmpty()) {
+                    p['c'] = JSON.stringify(input_cookies.toObject());
+                }
+                if (!input_headers.isEmpty()) {
+                    p['h'] = JSON.stringify(input_headers.toObject());
+                }
+                p['sign'] = genGetSign(p, keyinp.value);
+                post("/proxy/add", p, (data) => {
+                    let d = JSON.parse(data);
+                    let code = d['code'];
+                    if (code == -500) {
+                        console.warn('Server error: ', d['msg']);
+                        alert(i18n['SEVERR']);
+                    } else if (code == -401) {
+                        alert(i18n['UNAUTH']);
+                    } else if (code == 0) {
+                        alert(i18n['SUCADD']);
+                        get_proxy_list();
+                    } else {
+                        alert(d['msg']);
+                    }
+                })
+            } else {
+                alert(d['msg']);
+            }
+        })
+    })
+    fp.addEventListener('click', () => {
+        render_proxy_list(1);
+    })
+    pp.addEventListener('click', () => {
+        render_proxy_list(page_now - 1);
+    })
+    np.addEventListener('click', () => {
+        render_proxy_list(page_now + 1);
+    })
+    lp.addEventListener('click', () => {
+        let pc = Math.ceil(data.length / 10);
+        render_proxy_list(pc);
+    })
+    dab.addEventListener('click', () => {
+        if (!confirm(i18n['CODEA'] + '\n' + i18n['UNDOW'])) return;
+        let p = { "a": "deleteAll", "t": get_time() };
+        p['sign'] = genGetSign(p, keyinp.value);
+        post("/proxy/deleteAll", p, (da) => {
+            let d = JSON.parse(da);
+            let code = d['code'];
+            if (code == -500) {
+                console.warn('Server error: ', d['msg']);
+                alert(i18n['SEVERR']);
+            } else if (code == -401) {
+                alert(i18n['UNAUTH']);
+            } else if (code == 0) {
+                if (d['result']) {
+                    alert(i18n['SUCDEL']);
+                    get_proxy_list();
+                } else {
+                    alert(i18n['FADEL']);
+                }
             } else {
                 alert(d['msg']);
             }
